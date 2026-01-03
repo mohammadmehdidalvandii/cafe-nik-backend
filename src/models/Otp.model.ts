4c172fae54e8d1b4a6d8c7411dbce7ba51c7a88e
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2026-01-03 16:05:54 +0330

    created model otp

---
+++ src/models/Otp.model.ts
@@ -0,0 +1,30 @@
+import {DataTypes} from 'sequelize';
+import sequelize from '@/config/db.js';
+
+export const Otp = sequelize.define('Otp',{
+    id:{
+        type:DataTypes.UUID,
+        defaultValue:DataTypes.UUIDV4,
+        primaryKey:true,
+    },
+    phone:{
+        type:DataTypes.STRING(11),
+        allowNull:false,
+    },
+    code:{
+        type:DataTypes.STRING(6),
+        allowNull:false,
+    },
+    expires_at:{
+        type:DataTypes.DATE,
+        allowNull:false,
+    },
+    used:{
+        type:DataTypes.BOOLEAN,
+        defaultValue:false,
+    }
+},{
+    tableName:"otps",
+    underscored:true,
+    timestamps:true,
+});
