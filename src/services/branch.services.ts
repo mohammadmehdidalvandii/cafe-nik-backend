93beb9612e41978772026bd69c63bd4849bc86aa
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2026-01-06 09:53:11 +0330

    created branch.services

---
+++ src/services/branch.services.ts
@@ -0,0 +1,56 @@
+import {Branch} from '@/models/index.js';
+import { CreateBranchProps } from '@/types/branch.js';
+
+export const createBranch = async ({name , country , city_id , address , manager_name , phone}:CreateBranchProps)=>{
+    const branch = await Branch.create({
+        name,
+        country,
+        city_id,
+        address,
+        manager_name,
+        phone,
+    });
+     return branch
+};
+
+export const getAllBranches = async ()=>{
+    const branches = await Branch.findAll();
+    return branches
+};
+
+export const getBranchById = async (id:string)=>{
+    const branch = await Branch.findByPk(id);
+    return branch
+};
+
+export const updateBranch = async (id:string, data:Partial<{name:string , country:string , city_id:string , address:string , manager_name:string , phone:string}>)=>{
+    const branch = await Branch.findByPk(id);
+    if(!branch) throw new Error('این شعبه موجود نیست ');
+
+        const allowedFields:(keyof CreateBranchProps)[] = [
+        'name',
+        'city_id',
+        'country',
+        'address',
+        'manager_name',
+        'phone',
+    ];
+
+    let update = {} as any;
+
+    allowedFields.forEach((field)=>{
+        if(data[field] !== undefined){
+            update[field] = data[field]
+        }
+    })
+    await branch.update(update);
+    return branch
+};
+
+export const deleteBranch = async (id:string)=>{
+    const branch = await Branch.findByPk(id);
+    if(!branch) throw new Error('این شعبه وجود نداره');
+
+    const deleted = await branch.destroy();
+    return deleted
+}
--- src/types/branch.d.ts
+++ src/types/branch.d.ts
@@ -7,3 +7,12 @@ export interface BranchProps{
     manager_name:string,
     phone:string,
 }
+
+export interface CreateBranchProps{
+    name:string,
+    country:string,
+    city_id:string,
+    address:string,
+    manager_name:string,
+    phone:string,
+}
