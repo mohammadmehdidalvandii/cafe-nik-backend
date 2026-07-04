2f5e367324f20ded6e9b7a2730bc534738cf29ea
mohammadmehdi <mhmdmehdidalvandi@gmail.com>
2026-07-04 14:37:17 +0330

    fixed databse chabokan

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,11 @@
 import {Sequelize} from 'sequelize';
+import dotenv from "dotenv";
+dotenv.config();
 
-const sequelize = new  Sequelize("cafeNike" , "root", "mohammadmehdi79@",{
-    host:'localhost',
-    port:3306,
+
+const sequelize = new  Sequelize(process.env.NAME_DB! , process.env.USER_DB!, process.env.PASSWORD_DB,{
+    host:process.env.SERVER_DB,
+    port:Number(process.env.PORT_DB),
     dialect:'mysql',
     logging:false,
     define:{
